13e54ca251dec37a708637ff589ce7631073716b
jainshobhit231 <shobhit.mindiii@gmail.com>
2025-05-15 15:50:47 +0530

    integrate mvc structure

--- middleware/validation.js
+++ middleware/validation.js
@@ -1,6 +1,8 @@
 // Middleware to check name length
 const checkNameLength = (req, res, next) => {
   const { name } = req.body;
+
+  console.log("1st");
   if (!name) return res.status(400).json({ error: "Name is required" });
   console.log("Name Length:", name.length);
   if (name.length < 3)
