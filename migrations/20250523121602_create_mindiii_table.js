75f725192c89eff1a4d7bf3df63b9b6f538a4f50
shobhitjain1234 <shobhit10.jain@gmail.com>
2025-05-23 18:01:09 +0530

    working on migration

--- migrations/20250523121602_create_mindiii_table.js
+++ migrations/20250523121602_create_mindiii_table.js
@@ -3,14 +3,14 @@
  * @returns { Promise<void> }
  */
 exports.up = function (knex) {
-  return knex.schema.createTable("company", (table) => {
+  return knex.schema.createTable("mindiii", (table) => {
     table.increments("id").primary();
-    table.string("name").notNullable();
-    table.string("address");
+    table.string("employee_count").notNullable();
+    table.string("tech_count").unique().notNullable();
     table.timestamps(true, true); // created_at and updated_at
   });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTable("company");
+  return knex.schema.dropTable("mindiii");
 };
